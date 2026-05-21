3b6744a3e7f064d24137bed0ea184f7bfc0ab7af
aisehiwebwala <backw777@gmail.com>
2026-05-21 16:37:55 +0530

    feat: add railway configuration for build and deployment

--- BE/app.js
+++ BE/app.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 
 const api = require("./api")
 app.use("/api", api)
+
+const distPath = path.join(__dirname, "../FE/dist")
+app.use(express.static(distPath))
+app.get("*", (req, res) => res.sendFile(path.join(distPath, "index.html")))
 
 module.exports = app
