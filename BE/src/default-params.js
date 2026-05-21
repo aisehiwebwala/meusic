8930c4e63071d6f4a4d395575fd247ded5373160
aisehiwebwala <backw777@gmail.com>
2026-05-21 15:54:13 +0530

    refactor: enhance API setup with default headers and improve fetch function

--- BE/src/default-params.js
+++ BE/src/default-params.js
@@ -3,6 +3,7 @@ const default_params = {
     "_marker": 0,
     "api_version": 4,
     "ctx": "web6dot0",
+    "cc": "in"
 }
 
 module.exports = default_params
--- BE/src/setup/api-setup.js
+++ BE/src/setup/api-setup.js
@@ -1,7 +1,16 @@
 const API_BASE_URL = "https://www.jiosaavn.com/api.php"
 
-const FETCH = (_path, _config = { headers: { "User-Agent": "Mo" } }) => {
-    return fetch(`${API_BASE_URL}/${_path}`, _config)
+const DEFAULT_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
+    "Accept-Language": "en-IN,en;q=0.9,hi;q=0.8",
+    "Referer": "https://www.jiosaavn.com/",
+    "Origin": "https://www.jiosaavn.com"
 }
 
+const FETCH = (_path, _config = {}) => {
+    return fetch(`${API_BASE_URL}${_path}`, {
+        ..._config,
+        headers: { ...DEFAULT_HEADERS, ..._config.headers }
+    })
+}
 module.exports = FETCH
