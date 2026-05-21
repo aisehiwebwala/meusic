3037eda9e2cb631b19a58177bb32ec656458b79f
aisehiwebwala <backw777@gmail.com>
2026-05-21 11:51:51 +0530

    fix: correct usage of router in API routes

--- BE/api.js
+++ BE/api.js
@@ -2,12 +2,12 @@ const express = require("express")
 const router = express.Router()
 
 const search_route = require("./src/routes/search-route")
-app.use("/search", search_route)
+router.use("/search", search_route)
 
 const detail_route = require("./src/routes/detail.route")
-app.use("/detail", detail_route)
+router.use("/detail", detail_route)
 
 const meta_route = require("./src/routes/meta-route")
-app.use("/meta", meta_route)
+router.use("/meta", meta_route)
 
 module.exports = router
